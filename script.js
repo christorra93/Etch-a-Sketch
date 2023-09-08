ff8dbecce35288bf53cafccf1052086de71720a6
christorra93 <christorra93@gmail.com>
2023-09-07 21:39:48 -0300

    Added the function createGrid to the button

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ grid.style.height = `${SIZE}px`;
 grid.style.display = 'flex';
 grid.style.flexWrap = 'wrap';
 
-for(let i = 0; i< 16*16 ; i++){
+for(let i = 0 ; i< 16*16 ; i++){
     const square = document.createElement('div');
 
     square.style.width = `${SIZE/16}px`;
@@ -23,7 +23,7 @@ for(let i = 0; i< 16*16 ; i++){
 
 container.appendChild(grid);
 create.addEventListener('click',removeGrid);
-// create.addEventListener('click',createGrid);
+create.addEventListener('click',createGrid);
 grid.addEventListener('mouseover',changeColor);
 
 
@@ -37,6 +37,36 @@ function removeGrid(){
     if(grid){
         container.removeChild(grid);
     }
+}
+
+function createGrid(){
+    const MAX = 64;
+    let size = +prompt('Grid size?(MAX 64)','16');
+
+    size = (size > MAX) ? MAX : size; // if user input is>64 size takes the value of MAX
+
+    const grid = document.createElement('div');
+
+    const SIZE = 640;
+    grid.style.width = `${SIZE}px`;
+    grid.style.height = `${SIZE}px`;
+    grid.style.display = 'flex';
+    grid.style.flexWrap = 'wrap';
+
+    for(let i = 0 ; i < size*size ; i++){
+        const square = document.createElement('div');
+
+        square.style.width = `${SIZE/size}px`;
+        square.style.height = `${SIZE/size}px`;
+        square.style.border = '1px solid black';
+        square.style.boxSizing = 'border-box';
+
+        grid.appendChild(square);
+    }
+    
+    grid.addEventListener('mouseover',changeColor);
+
+    container.appendChild(grid);
 }
 
 
