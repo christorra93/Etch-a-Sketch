d61f7fe7396e31cc343d2225adee0cec13cbdb43
christorra93 <christorra93@gmail.com>
2023-09-07 23:50:36 -0300

    Added an event for the clear button

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const container = document.querySelector('#container');
+
+//BUTTONS
 const create = document.querySelector('#gridbtn');
 const rgb = document.querySelector('#rgb');
 const grad = document.querySelector('#gradient');
+const clear = document.querySelector('#clear');
+//
+
 const grid = document.createElement('div');
 
 const SIZE = 640;
@@ -30,9 +35,28 @@ rgb.addEventListener('click',addRgb);
 
 grad.addEventListener('click',addGradient);
 
+clear.addEventListener('click',clearGrid);
+
+function clearGrid(){
+    const grid = document.querySelector('#container div');
+    const square = document.querySelectorAll('#container div div');
+
+    square.forEach(element => {
+        element.style.backgroundColor = 'white';
+        element.style.opacity = 1;
+    })
+
+    grid.removeEventListener('mouseover',rgbColor);
+    grid.removeEventListener('mouseover',gradient);
+
+    grid.addEventListener('mouseover',changeColor);
+
+}
+
 function addRgb(){
     const grid = document.querySelector('#container div');
 
+    grid.removeEventListener('mouseover',gradient);
     grid.removeEventListener('mouseover',changeColor);
     grid.addEventListener('mouseover',rgbColor);
 }
@@ -55,6 +79,7 @@ function addGradient(){
         element.style.opacity = '0';
     })
 
+    grid.removeEventListener('mouseover',rgbColor);
     grid.removeEventListener('mouseover',changeColor);
     grid.addEventListener('mouseover',gradient);
 }
@@ -63,7 +88,7 @@ function gradient(e){
     opa+=0.1;
 
     e.target.style.opacity = `${opa}`;
-    
+
 }
 function changeColor(e){
     e.target.style.backgroundColor = 'black';
